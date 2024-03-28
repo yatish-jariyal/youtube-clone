09437e3ccbd7572f590b4618c0b8e808cf366c84
Yatish Jariyal <yatish@Yatishs-MacBook-Air.local>
2024-03-28 13:40:19 +0530

    Updated

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Stack, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import { logo } from "../utils/constants";
+import SearchBar from "./SearchBar";
+
+const Navbar = () => (
+  <Stack
+    direction="row"
+    alignItems="center"
+    p={2}
+    sx={{
+      position: "sticky",
+      background: "#000",
+      top: 0,
+      justifyContent: "space-between",
+    }}
+  >
+    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+      <img src={logo} alt="logo" height={45} />
+      <Typography variant="h6" fontWeight="bold" color="#fff" ml={1}>
+        YouTube
+      </Typography>
+    </Link>
+    <SearchBar />
+  </Stack>
+);
+
+export default Navbar;
